9b7912caa2f09b88673fc082d2a9d9ed971162f6
FrontWalker2000 <935145806@qq.com>
2021-05-23 19:39:43 +0800

    commit

--- sever/config.js
+++ sever/config.js
@@ -2,7 +2,7 @@ const config = {
     database: 'vueadmin',
     host: 'localhost',
     port: 3306,
-    user: 'root',
-    password: 'yangjialin123'
+    user: 'root',// 改成自己的
+    password: 'yangjialin123' // 改成自己的
 }
 module.exports = config
